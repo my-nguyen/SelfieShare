0da4c6c9c2e7ce0f54c2aa9880da97416a3d7baa
My Nguyen <My@mys-macbook-pro.local>
2016-08-17 14:11:08 -0700

    Importing photos again

--- SelfieShare/ViewController.swift
+++ SelfieShare/ViewController.swift
@@ -8,11 +8,18 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
+
+    @IBOutlet var collectionView: UICollectionView!
+    var images = [UIImage]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        // give the title a name
+        title = "Selfie Share"
+        // add a right bar button with the system's camera icon
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .Camera, target: self, action: #selector(importPicture))
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +27,52 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    // return the number of cells in the collection view
+    func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return images.count
+    }
 
+    func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        // fetch the cell with reuse identifier "ImageView"
+        let cell = collectionView.dequeueReusableCellWithReuseIdentifier("ImageView", forIndexPath: indexPath)
+
+        // fetch the image view with tag 1000 inside the cell
+        if let imageView = cell.viewWithTag(1000) as? UIImageView {
+            imageView.image = images[indexPath.item]
+        }
+
+        return cell
+    }
+
+    // see project NamesToFaces, method ViewController.addNewPerson() for a detailed description
+    func importPicture() {
+        let picker = UIImagePickerController()
+        picker.allowsEditing = true
+        picker.delegate = self
+        presentViewController(picker, animated: true, completion: nil)
+    }
+
+    // see project NamesToFaces for a detailed description of this method
+    func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : AnyObject]) {
+        var newImage: UIImage
+
+        if let possibleImage = info[UIImagePickerControllerEditedImage] as? UIImage {
+            newImage = possibleImage
+        } else if let possibleImage = info[UIImagePickerControllerOriginalImage] as? UIImage {
+            newImage = possibleImage
+        } else {
+            return
+        }
+
+        dismissViewControllerAnimated(true, completion: nil)
+
+        images.insert(newImage, atIndex: 0)
+        collectionView.reloadData()
+    }
+
+    // see project NamesToFaces for a detailed description of this method
+    func imagePickerControllerDidCancel(picker: UIImagePickerController) {
+        dismissViewControllerAnimated(true, completion: nil)
+    }
 }
 
